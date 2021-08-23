0d120dfaa15aa6bfc4346a20569a6cc879b53142
Max <max@maxg.dev>
2021-08-23 14:21:09 +0100

    Remove testMain test.

--- src/test/java/dev/maxg/blackjack/BlackjackGameTest.java
+++ src/test/java/dev/maxg/blackjack/BlackjackGameTest.java
@@ -38,18 +38,6 @@ public class BlackjackGameTest {
     }
 
     /**
-     * Test of main method, of class BlackjackGame.
-     */
-    @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        BlackjackGame.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of dealCards method, of class BlackjackGame.
      */
     @Test
